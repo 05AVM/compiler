8e9435c9053c05fd9de12ae059dad237e18255e1
Avishek <avishekmondal923@gmail.com>
2023-10-08 01:45:20 +0530

    Avishek

--- src/App.js
+++ src/App.js
@@ -1,55 +1,105 @@
-import React, { useState } from 'react';
+import { useEffect, useState } from 'react';
 import axios from 'axios';
+import "./App.css";
+import AceEditor from "react-ace";
+import "ace-builds/src-noconflict/mode-java";
+import "ace-builds/src-noconflict/mode-csharp";
+import "ace-builds/src-noconflict/mode-python";
+import "ace-builds/src-noconflict/theme-dracula";
 
 function App() {
   const [code, setCode] = useState('');
-  const [language, setLanguage] = useState('cpp');
+  const [language, setLanguage] = useState('java');
   const [output, setOutput] = useState('');
   const [error, setError] = useState('');
+  const [input, setInput] = useState('');
+  const [mode, setMode] = useState('java')
 
-  const handleCompile = async () => {
-    try {
-      const response = await axios.post('https://api.codex.jaagrav.in', {
-        code,
-        language,
-        input: '7', // You can modify the input as needed
+  const handleCompile = () => {
+
+    axios.post('http://localhost:4000/compile', {
+      code: code,
+      language: language,
+      input: input
+    })
+      .then((response) => {
+        console.log(response.data);
+        setOutput(response.data.output);
+        setError(response.data.error);
+      })
+      .catch((error) => {
+        setError(error.message);
       });
-
-      // Handle the response from the external API here
-      console.log('Response Data:');
-      console.log(JSON.stringify(response.data));
-
-      // Update the state with the response data
-      setOutput(response.data.output);
-      setError(response.data.stderr);
-    } catch (error) {
-      console.error(error);
-      setError('An error occurred during code execution.');
-    }
   };
 
+  useEffect(() => {
+    switch (language) {
+      case "c":
+        setMode('java');
+        break;
+      case "cpp":
+        setMode('java');
+        break;
+      case "py":
+        setMode('python');
+        break;
+      case "java":
+        setMode('java');
+        break;
+      case "cs":
+        setMode('csharp');
+        break;
+      default:
+        setMode('python');
+        break;
+    }
+  }, [language])
   return (
-    <div className="App">
+    <div className="App" >
       <h1>Code Compiler</h1>
-      <textarea
-        name='code'
-        placeholder="Enter your code here"
-        value={code}
-        onChange={(e) => setCode(e.target.value)}
-      ></textarea>
-      <select onChange={(e) => setLanguage(e.target.value)} value={language} name='language'>
-        <option value="cpp">C++</option>
-        <option value="python">Python</option>
-        {/* Add more language options as needed */}
-      </select>
-      <button onClick={handleCompile}>Compile</button>
-      <div>
-        <h2>Output:</h2>
-        <pre>{output}</pre>
-      </div>
-      <div>
-        <h2>Error:</h2>
-        <pre>{error}</pre>
+      <div style={{ 'display': 'flex' }}>
+        <div>
+          <AceEditor
+            mode={mode}
+            theme='dracula'
+            setOptions={{
+              enableBasicAutocompletion: true,
+              enableLiveAutocompletion: true,
+              enableSnippets: true,
+              fontSize: 20,
+              showPrintMargin: false,
+            }}
+            onChange={(e) => setCode(e)}
+          />
+        </div>
+        <div>
+          <select onChange={(e) => setLanguage(e.target.value)} name='language'>
+            <option value="py">Python</option>
+            <option value="c">C</option>
+            <option value="cpp">C++</option>
+            <option value="cs">C#</option>
+            <option value="java">Java</option>
+            <option value="js">Javascript</option>
+            <option value="go">Golang</option>
+          </select>
+          <button onClick={handleCompile}>Compile</button><br />
+          <label htmlFor='code-input'>Input </label>
+          <textarea
+            id='code-input'
+            type='text'
+            placeholder='Enter input If required'
+            value={input}
+            onChange={(event) => setInput(event.target.value)}
+          />
+          <div>
+            <h2>Output:</h2>
+            <pre>{output}</pre>
+          </div>
+          <div>
+            <h2>Error:</h2>
+            <pre>{error}</pre>
+          </div>
+        </div>
       </div>
     </div>
   );
